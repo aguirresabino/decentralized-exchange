ae2a5645f23f5eb8d1a313a20340c66f181a185c
Aguirre Sabino <aguirresabino187@gmail.com>
2021-07-29 03:29:48 -0300

    chore(): configure client React

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/index.scss';
+import { App } from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
